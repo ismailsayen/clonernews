8314cc3a95f387a38df76adf71cdbeef0691a439
aayoubst <ayoubstudies01@gmail.com>
2025-03-10 16:41:47 +0000

    finish at home

--- index.js
+++ index.js
@@ -1,30 +1,75 @@
-const defaultData = {
-  tabs: "Stories",
-  items: [],
-};
+let tab = document.querySelectorAll(".tabs li");
+let notificationBtn = document.querySelector(".notificationBtn");
+let creatUrl = (id)=> `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+let cardsContainer = document.querySelector(".cardsContainer");
+
+notificationBtn.addEventListener("click", () => {
+  location.reload();
+});
 
 const run = async () => {
-  let tab = document.querySelectorAll(".tabs li");
   let lastId = await getData(
     "https://hacker-news.firebaseio.com/v0/maxitem.json"
   );
-  console.log(lastId);
+
+  let lastFetchedId = lastId
+
+  async function loadData() {
+    let counter = 0 
+    let cards = []
+    let loader = document.querySelector(".loading")
+    loader.classList.remove("invisible")
+    while (counter < 10) {
+      let data = await getData(creatUrl(lastFetchedId));
+      lastFetchedId--;
+      if (data.type !== "comment" && !data.dead){
+        cards.push(createCard(data))
+        counter++;
+      }
+    }
+
+    loader.classList.add("invisible")
+    cards.forEach(element => {
+      cardsContainer.appendChild(element)
+    });
+    setInterval(async () => {
+      let data = await getData(
+        `https://hacker-news.firebaseio.com/v0/maxitem.json`
+      );
+      if (data !== lastId) {
+        lastId = data;
+        notificationBtn.style.backgroundColor = "green";
+      }
+    },5000)
+  }
+  loadData()
+document.addEventListener("scroll", () => {
+  loadData()
+})
+  function createCard(data) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.classList.add("card");
+    let title = document.createElement("h3");
+    title.textContent = data.title;
+    let url = document.createElement("a");
+    let type = document.createElement("p");
+    type.textContent = data.type
+    url.href = data.url;
+    url.textContent = data.url;
+    card.appendChild(title);
+    card.appendChild(url);
+    card.appendChild(type);
+    return card
+    
+  }
+
 
   tab.forEach((li) => {
     li.addEventListener("click", () => {
       defaultData.tabs = li.textContent;
-      fetchData();
     });
   });
-
-  async function fetchData() {
-    switch (defaultData.tabs) {
-      case "Stories":
-        getData("https://hacker-news.firebaseio.com/v0/topstories.json");
-      case "Jobs":
-        getData("https://hacker-news.firebaseio.com/v0/jobstories.json");
-    }
-  }
 
   async function getData(url) {
     let resp = await fetch(url);
@@ -32,4 +77,5 @@ const run = async () => {
     return json;
   }
 };
+
 run();
