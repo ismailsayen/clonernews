be0c336c1ba6de2fc1aa70dee0d620338cb4ec2f
isayen <sayensma02@gmail.com>
2025-03-10 11:39:04 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const defaultData = {
+  tabs: "Stories",
+  items: [],
+};
+let tab = document.querySelectorAll(".tabs li");
+
+tab.forEach((li) => {
+  li.addEventListener("click", () => {
+    defaultData.tabs = li.textContent;
+    fetchData();
+    console.log(defaultData.tabs);
+  });
+});
+const fetchData = () => {
+  switch (defaultData.tabs) {
+    case "Stories":
+      fetchStories("https://hacker-news.firebaseio.com/v0/topstories.json");
+    case "Jobs":
+      fetchStories("https://hacker-news.firebaseio.com/v0/jobstories.json");
+  }
+};
+
+const fetchStories = async (url) => {
+  let ids = [];
+  try {
+    let resp = await fetch(url);
+    const json = await resp.json();
+    ids = json;
+    console.log(defaultData.tabs, ids);
+  } catch {
+    console.log("error");
+  }
+};
+fetchData();
