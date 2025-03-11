f517b100dea8ba5945701aa16ca84ab7fdb849a6
DarkMethoss <ayoubstudies01@gmail.com>
2025-03-11 20:27:23 +0000

    .

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let tab = document.querySelectorAll(".tabs li");
+let tabValue = 'story'
 let notificationBtn = document.querySelector(".notificationBtn");
 let creatUrl = (id) => `https://hacker-news.firebaseio.com/v0/item/${id}.json`;
 let cardsContainer = document.querySelector(".cardsContainer");
@@ -8,6 +9,8 @@ const windowHasScroll = () =>
 notificationBtn.addEventListener("click", () => {
   location.reload();
 });
+
+
 const convertTime = (time) => {
   let secondes = Math.floor(time / 1000);
   if (secondes < 60) return `${secondes} secondes ago.`;
@@ -18,6 +21,14 @@ const convertTime = (time) => {
   let jours = Math.floor(heures / 24);
   return `${jours} days ago.`;
 };
+
+tab.forEach((li) => {
+  li.addEventListener("click", () => {
+    cardsContainer.innerHTML = ""
+    tabValue = li.id
+    run();
+  })
+})
 
 // Fixed throttle function
 function throttle(func, wait) {
@@ -52,7 +63,7 @@ const run = async () => {
       let data = await getData(creatUrl(lastFetchedId));
       lastFetchedId--;
 
-      if (data && data.type !== "comment" && !data.dead) {
+      if (data && data.type === tabValue && !data.dead) {
         createCard(data);
         counter++;
       }
@@ -78,30 +89,66 @@ const run = async () => {
 
   function createCard(data) {
     let card = document.createElement("div");
-    card.classList.add("card");
-    let title = document.createElement("h3");
-    title.textContent = data.title;
     let url = document.createElement("a");
-    let type = document.createElement("p");
+    let title = document.createElement("h3");
+    let cmtBtn = document.createElement("button")
+    let cmtContainer = document.createElement("div")
     let time = document.createElement("p");
-
-    type.textContent = data.type;
-    url.href = data.url;
+    let type = document.createElement("p");
+    card.classList.add("card");
+    title.textContent = data.title;
     url.textContent = data.url;
+    url.href = data.url;
+    url.setAttribute('target', '_blank');
+    url.style.display = "block"
+    cmtBtn.classList.add('cmtBtn')
+    cmtBtn.textContent = "Show Comments"
+    cmtContainer.classList.add('cmtsContainer')
     time.textContent = convertTime(data.time);
+    type.textContent = data.type;
+    card.setAttribute('data-id', data.id);
+    card.setAttribute('data-kids', data.kids?.join(','))
     card.appendChild(title);
     card.appendChild(url);
+    card.appendChild(cmtBtn);
+    card.appendChild(cmtContainer);
     card.appendChild(type);
     card.appendChild(time);
-
     cardsContainer.appendChild(card);
+
+    cmtBtn.addEventListener("click", () => {
+      fetchComments(card)
+    })
   }
 
-  tab.forEach((li) => {
-    li.addEventListener("click", () => {
-      defaultData.tabs = li.textContent;
-    });
-  });
+  function fetchComments(card) {
+    let cardId = card.dataset.id
+    let commentsIds = card.dataset.kids === "undefined" ? undefined : card.dataset.kids.split(",")
+    let cmtContainer = card.querySelector('.cmtsContainer')
+    let cmtClassList = cmtContainer.classList
+    cmtContainer.textContent = "No Comments ..."
+    if (!commentsIds) cmtClassList.add('show');
+    if (cmtClassList.contains('hide')){
+      cmtClassList.replace('hide', 'show')
+      card.querySelector('.cmtBtn').textContent = "Hide Comments"
+    } else if (cmtClassList.contains('show')){
+      cmtClassList.replace('show', 'hide')
+      card.querySelector('.cmtBtn').textContent = "Show Comments"
+    } else {
+      commentsIds.forEach(async cmtId => {
+        let res = await getData(creatUrl(cmtId))
+        if (res.parent === cardId && !res.dead && !res.deleted) {
+          // if (cmtContainer.textContent === "No Comments ...") cmtContainer.innerHTML = ""; 
+          let comment = document.createElement('div')
+          comment.classList.add('comment')
+          comment.innerHTML = res.text
+          cmtContainer.append(comment)
+        }
+        console.log(res)
+      });
+      cmtClassList.add('hide')
+    }
+  }
 
   async function getData(url) {
     try {
